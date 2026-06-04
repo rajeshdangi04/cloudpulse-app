215b9665c23fe4cb1993ba4c42e35250ed3b1105
Rajesh <rajeshdangi445566@gmail.com>
2026-06-04 18:22:17 +0530

    v4

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    version = os.getenv("APP_VERSION", "3.0")
+    version = os.getenv("APP_VERSION", "4.0")
     return render_template("index.html", version=version)
 
 
